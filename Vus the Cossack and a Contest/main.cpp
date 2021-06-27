41d50b31c5b707c9a9cf08c753549c38538cf077
Tuan Anh Ho <tommyho@Tuans-MacBook-Air.local>
2021-06-27 18:04:07 +0900

    Adding Vus the cossack and a contest

---
+++ Vus the Cossack and a Contest/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << (m >= n && k >= n ? "Yes\n" : "No\n");
+}
+
+
+
+int main()
+{
+    solve();
+
+}
