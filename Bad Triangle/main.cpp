8df5e948ffec20023328c369aa5de356e49d2394
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-02 15:58:48 +0900

    adding bad triangle

---
+++ Bad Triangle/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &x : v) cin >> x;
+
+    if(v[0] + v[1] > v[n-1]) 
+       cout << -1 << endl; 
+    else
+        cout << "1 2 " << n << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
