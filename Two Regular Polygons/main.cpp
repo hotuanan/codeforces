169f4ce009160dc654031cef9c527b7f4962a8ad
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-08 21:11:26 +0900

    Adding two regular polygons

---
+++ Two Regular Polygons/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n, m;
+    cin >> n >> m;
+
+    if(n % m == 0) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
