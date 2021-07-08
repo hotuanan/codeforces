bba3c925decccc3a633f5903fa7c0cd28a9f177b
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-08 14:28:38 +0900

    adding strange functions:

---
+++ Strange Functions/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    string a;
+    cin >> a;
+    cout << a.length() << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) 
+        solve();
+}
