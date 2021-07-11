a7298e5301985569db835a1d15e17c1c66e101af
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-11 17:27:31 +0900

    adding potion making, but didnt really solve it

---
+++ Potion-making/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+void solve() 
+{
+    int k;
+    cin >> k;
+    cout << 100/ gcd(100, k) << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
