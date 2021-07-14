c16934356b18b8f08962967e332fdb9b8f5ad2ea
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-14 15:05:40 +0900

    adding last minute enhancements

---
+++ Last minute enhancements/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int h[1000001] = {0};
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    // int m = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        h[v[i]]++;
+        // m = max(m, v[i]);
+    }
+
+    int counter = 1;
+    int last = v[0];
+    for(int i = 1; i < n; i++) {
+        if(v[i] == last) {
+            v[i]++;
+            counter++;
+            last = v[i];
+        }
+        else if(v[i] > last) {
+            counter++;    
+            last = v[i];
+        } 
+    }
+
+    cout << endl;
+    cout << counter << endl;
+
+
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
