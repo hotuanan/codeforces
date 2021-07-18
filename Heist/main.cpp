f37ca165311c77199557fa4afac991265619525b
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-18 14:12:37 +0900

    adding heist

---
+++ Heist/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &x : v) cin >> x;
+    sort(v.begin(), v.end());
+    int res = 0;
+    for(int i = 0; i < n - 1; i++) {
+        if(v[i + 1] != v[i] + 1) res += v[i + 1] - v[i] - 1;
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
