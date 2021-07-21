b7d788274b79ea77e0e57b3e090a4ebffa384cf9
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-21 16:48:56 +0900

    adding integer sequence dividing

---
+++ Integer Sequence Dividing/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    if(((n + 1) / 2) % 2 & 1) cout << 1 << endl;
+    else cout << 0 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
