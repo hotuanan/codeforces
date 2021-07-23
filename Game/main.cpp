a7b8d7a2ddea32333b7a035a9eeceedd885216f6
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-23 09:55:49 +0200

    adding game

---
+++ Game/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n1, n2, k1, k2;
+    cin >> n1 >> n2 >> k1 >> k2;
+    if(n1 > n2) cout << "First\n";
+    else cout << "Second\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
