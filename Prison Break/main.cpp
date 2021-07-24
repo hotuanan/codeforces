eeae9277c1995de65bb6dceb10c32470b7ca3c84
Tuan Anh Ho <qtomas64@gmail.com>
2021-07-24 23:04:52 +0200

    Adding prison break

---
+++ Prison Break/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n, m;
+    cin >> n >> m;
+    cout << n * m << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
