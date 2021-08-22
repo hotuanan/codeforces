b750adea642c44ea49e1134060020a5dc6d0f359
Tuan Anh Ho <qtomas64@gmail.com>
2021-08-22 14:30:03 +0200

    Adding Gregor and cryptography

---
+++ Gregor and Cryptography/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int p;
+    cin >> p;
+    cout << 2 << " " << p - 1 << endl;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
