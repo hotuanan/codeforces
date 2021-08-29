d158337fa6f80badfbf46e60171d55cb5b5138c4
Tuan Anh Ho <qtomas64@gmail.com>
2021-08-29 13:45:33 +0200

    adding equation

---
+++ Equation/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    cout << 9*n << " " << 8*n << endl;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
