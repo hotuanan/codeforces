dfc0934090c9b2aa79fbfb49666c1208c83370ac
Tuan Anh Ho <qtomas64@gmail.com>
2021-10-06 11:56:21 +0200

    adding remove a progression

---
+++ Remove a Progression/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n, x;
+    cin >> n >> x;
+    cout << 2 * x << endl;
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
