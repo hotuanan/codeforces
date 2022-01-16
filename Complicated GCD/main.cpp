94701869ba7670461b694fb4ef325ed3a11f225a
Tuan Anh Ho <qtomas64@gmail.com>
2022-01-16 13:38:50 +0100

    Adding complicated gcd

---
+++ Complicated GCD/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    string a,b;
+    cin >> a >> b;
+    if(a == b) cout << a << endl;
+    else cout << 1 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
