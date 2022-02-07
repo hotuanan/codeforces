1d6928ad28216f11cdf33802ee731b8100738baf
Tuan Anh Ho <qtomas64@gmail.com>
2022-02-07 11:24:09 +0100

    Adding definite game

---
+++ 1081A - Definite Game/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+
+    if (n < 3) cout << n << endl;
+    else cout << 1 << endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
