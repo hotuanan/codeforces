da5599e6f04084c4904e383afab15044df3ef0b4
Tuan Anh Ho <qtomas64@gmail.com>
2022-02-08 11:43:54 +0100

    adding little artem and presents

---
+++ 669A - Little Artem and Presents/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+
+    cout << 2 * (n / 3) + ((n % 3) > 0) << endl;
+    
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
