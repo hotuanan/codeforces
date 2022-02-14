e78552bfe6ca4b13db1aa02159f97a9e43c508c4
Tuan Anh Ho <qtomas64@gmail.com>
2022-02-14 10:31:34 +0100

    adding abc

---
+++ 1632A - ABC/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    string a;
+    cin >> a;
+
+    if (a == "0" || a == "1" || a == "01" || a == "10") cout << "YES\n";
+    else cout << "NO\n";
+    
+
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
