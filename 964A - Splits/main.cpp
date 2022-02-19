95f1f99c5e2ab469ea939df0188255f3ccceb520
Tuan Anh Ho <qtomas64@gmail.com>
2022-02-19 16:04:18 +0100

    adding splits

---
+++ 964A - Splits/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    
+    int max_k = n / 2;
+    cout << max_k + 1 << endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    // cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
