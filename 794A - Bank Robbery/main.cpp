8fdeaf0f41041ce1a267ec0b71c1234da6c25db5
Tuan Anh Ho <qtomas64@gmail.com>
2022-03-22 15:05:40 +0100

    adding bank robbery

---
+++ 794A - Bank Robbery/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int me, g1, g2;
+    cin >> me >> g1 >> g2;
+    int n;
+    cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        
+        if (tmp > g1 && tmp < g2) res++;
+    }
+    cout << res << endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
