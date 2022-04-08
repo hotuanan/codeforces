33bae7e8554f1d08958c51893a9f8b1167840c06
Tuan Anh Ho <qtomas64@gmail.com>
2022-04-08 13:46:31 +0200

    Adding playoff

---
+++ 1651A - Playoff/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    cout << (int)pow(2, n) - 1 << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
