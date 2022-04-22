d0add9bc95a3ce537456ca92fcffa0379a331d1c
Tuan Anh Ho <qtomas64@gmail.com>
2022-04-22 10:19:48 +0200

    adding gcd vs lcm

---
+++ 1665A - GCD vs LCM/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    cout << n - 3 << " 1 1 1\n";
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
