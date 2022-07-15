91dcd455c3ee4ee5512534b69f18e3a69ec74b5f
hotuanan <ho.tuan.tomas@gmail.com>
2022-07-15 10:37:43 +0200

    adding hexadecimals theorem

---
+++ 199A - Hexadecimal's theorem/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << "0 0 " << n << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
