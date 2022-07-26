382bf5c5c1c51935d7fd4bc5cadcc87c7d6a2f25
Tuan Anh Ho <qtomas64@gmail.com>
2022-07-26 13:15:29 +0200

    Adding array stabilization

---
+++ 1095B - Array Stabilization/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+ 
+void solve() 
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << min(v[n - 2] - v[0], v[n - 1] - v[1]) << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+}
